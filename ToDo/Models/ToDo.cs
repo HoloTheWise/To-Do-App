034e0f14343ff4cfdec0da68a7097e4e0e7389b5
HoloTheWise <yusufcem1811@gmail.com>
2024-08-07 10:00:18 +0300

    Initial commit

---
+++ ToDo/Models/ToDo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoDemo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Açıklama giriniz.")]
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Tarih giriniz")]
+
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Kategori seçiniz.")]
+
+        public string CategoryId { get; set; } = string.Empty;
+
+        [ValidateNever]
+
+        public Category Category { get; set; } = null!;
+
+       [Required(ErrorMessage = "Durum seçiniz.")]
+
+        public string StatusId { get; set; } = string.Empty;
+
+        [ValidateNever]
+
+        public Status Status { get; set; } = null!;
+
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today; 
+
+
+    }
+}
---
+++ ToDo/Models/ToDoContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoDemo.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Status> Statuses { get; set; } = null!;
+
+
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = "is", Name="İş"},
+                new Category { CategoryId = "ev", Name = "Ev" },
+                new Category { CategoryId = "okul", Name = "Okul" },
+                new Category { CategoryId = "ex", Name = "Exercise" },
+                new Category { CategoryId = "shop", Name = "Alışveriş" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "open", Name = "Open" },
+                new Status { StatusId = "closed", Name = "Completed" }
+                );
+        }
+    }
+}
